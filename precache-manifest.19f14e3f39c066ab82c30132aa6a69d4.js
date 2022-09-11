4d0e0139483cb9a211a89f406bef6bd783167a98
raphmercp <raphmercp@gmail.com>
2022-09-11 13:29:49 -0600

    Updates

---
+++ precache-manifest.19f14e3f39c066ab82c30132aa6a69d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d64725e35e04f99c388",
+    "url": "/game-of-life/static/css/main.a342a919.chunk.css"
+  },
+  {
+    "revision": "5d64725e35e04f99c388",
+    "url": "/game-of-life/static/js/main.5d64725e.chunk.js"
+  },
+  {
+    "revision": "0b5adef5a8e99730695f",
+    "url": "/game-of-life/static/js/1.0b5adef5.chunk.js"
+  },
+  {
+    "revision": "8973c619eab5129aa76e",
+    "url": "/game-of-life/static/js/2.8973c619.chunk.js"
+  },
+  {
+    "revision": "8d1840913a43c8b0b93f",
+    "url": "/game-of-life/static/js/runtime~main.8d184091.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/game-of-life/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "87ab5ee9693f40adcd2975778512705c",
+    "url": "/game-of-life/index.html"
+  }
+];
